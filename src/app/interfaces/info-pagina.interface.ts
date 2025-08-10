411f461553ea0619af719781af4017a3e006836d
Jose Fernandez <alexelcrac67@gmail.com>
2025-08-10 11:52:09 -0400

    Verificando Interfaces

--- src/app/interfaces/info-pagina.interface.ts
+++ src/app/interfaces/info-pagina.interface.ts
@@ -3,7 +3,7 @@
 // Aqui esta el modelo de la información de la página asi como se define en el JSON
 // Este modelo se utiliza para tipar la información que se obtiene del servicio InfoPaginaService
 // Usamos una de las extensiones para generar interfaces a partir del archivo JSON que creamos con crtl + shift + p -> TypeScript: Generate interface from JSON
-// El signo de interrogación (?) indica que la propiedad es opcional puede que se use como no.
+// El signo de interrogación (?) indica que la propiedad es opcional puede que se use como no en la pagina .
 export interface InfoPagina {
   titulo?: string;
   email?: string;
